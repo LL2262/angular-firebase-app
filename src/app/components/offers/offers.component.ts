3229d41d1b98f1e0845176d730e9ed7d15af3b9f
luciano lamonica <lucho_bio@hotmail.com>
2018-12-20 17:13:04 -0300

    Primer commit

---
+++ src/app/components/offers/offers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { DataApiService } from '../../services/data-api.service';
+import { BookInterface } from '../../models/book-interface';
+
+@Component({
+  selector: 'app-offers',
+  templateUrl: './offers.component.html',
+  styleUrls: ['./offers.component.css']
+})
+export class OffersComponent implements OnInit {
+  constructor(private dataApi: DataApiService) { }
+  private books: BookInterface;
+
+  ngOnInit() {
+  }
+}
